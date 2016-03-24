a861a0acaf51982be0b4f9d6f36668f77c46de47
Hannahcp822 <hannahpang@Hannahs-Laptop.local>
2016-03-24 00:47:10 -0700

    Elevens - 03/23/16

--- ElevensStudentStarterCode/Activity5  Starter Code/Buggy1/DeckTester.java
+++ ElevensStudentStarterCode/Activity5  Starter Code/Buggy1/DeckTester.java
@@ -1,3 +1,5 @@
+package Buggy1;
+
 /**
  * This is a class that tests the Deck class using assert statements.
  */
@@ -92,7 +94,7 @@ public class DeckTester {
 	 * Check that the given deck is empty.
 	 * @param d is a deck that should be empty.
 	 */
-	private static void testEmpty(Deck d) {
+	private static void testEmpty(Deck d) { 
 		assert d.size() == 0 :  "Size for an empty deck is " + d.size()
 			+ ". It should be 0.";
 		assert d.isEmpty() : "isEmpty is false for an empty deck.";
